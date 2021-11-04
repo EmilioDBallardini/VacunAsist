79668a05fc93e7086a82583b2bdf0e47fe3c4c80
qudit <qudit@altair.infinite>
2021-11-04 18:29:49 -0300

    cambios en las noticias, routes y ruby-version

--- app/controllers/noticium_controller.rb
+++ app/controllers/noticium_controller.rb
@@ -9,7 +9,7 @@ class NoticiumController < ApplicationController
 	end
 
 	def create
-        @noticium = Noticium.new(params.require(:noticium).permit(:titulo,:subtitulo,:epigrafe,:img_url,:cuerpo))
+        @noticium = Noticium.new(params.permit(:noticium))
 
         if @noticium.save
             redirect_to home_path, notice: "La noticia se cargó satisfactoriamente"
@@ -17,10 +17,23 @@ class NoticiumController < ApplicationController
             flash[:error] = "Hubo un error al cargar la noticia"
             render :new
         end
+
+	end
+
+	def update
+        @noticium = noticium.find(params[:id])
+
+        if @noticium.update(params.require(:noticium).permit(:titulo, :epigrafe, :pie_de_pagina, :cuerpo, :subtitulo))  
+            redirect_to home_path, notice: "La noticia se editó satisfactoriamente"
+        else
+            flash[:error] = "Hubo un error al editar la noticia"
+            render :edit
+        end
 	end
 
 
 	def show
+		@noticium = Noticium.find(params[:id])
 	end
 
 	def destroy
--- app/models/noticium.rb
+++ app/models/noticium.rb
@@ -1,2 +1,5 @@
 class Noticium < ApplicationRecord
+
+
+	
 end
