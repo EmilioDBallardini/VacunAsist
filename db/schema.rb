8f464f0171c3e9516212fff7fd28fb65c9887284
Cristian <cristianriverasd@gmail.com>
2021-11-16 13:39:49 -0300

    Turnos por vacunatorio

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_14_020442) do
+ActiveRecord::Schema.define(version: 2021_11_16_161815) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -43,6 +43,8 @@ ActiveRecord::Schema.define(version: 2021_11_14_020442) do
     t.boolean "disponible"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "vaccination_id", null: false
+    t.index ["vaccination_id"], name: "index_turnos_on_vaccination_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -70,4 +72,5 @@ ActiveRecord::Schema.define(version: 2021_11_14_020442) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "turnos", "vaccinations"
 end
