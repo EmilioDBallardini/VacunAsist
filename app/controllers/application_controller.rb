f41c8cb110a5788f80875f38feaa343cd8623845
Sofia <sofiapeinetti@gmail.com>
2021-11-17 15:34:51 -0300

    Ver/modificar perfil y contraseña terminados

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,8 @@ class ApplicationController < ActionController::Base
 
   protected
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nombre, :apellido, :dni, :nacimiento, :email])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:nombre, :apellido, :dni, :nacimiento, :email])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nombre, :apellido, :dni, :nacimiento, :email, :enfermedad])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:nombre, :apellido, :dni, :nacimiento, :email, :enfermedad])
     devise_parameter_sanitizer.permit(:sign_in, keys: [:email, :password])
   end
 
