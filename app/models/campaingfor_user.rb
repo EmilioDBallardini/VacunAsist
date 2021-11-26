c58f5ef5d5a68eb7f9ab133c4128e87b41478017
Cristian <cristianriverasd@gmail.com>
2021-11-25 21:53:55 -0300

    turno asignado?

--- app/models/campaingfor_user.rb
+++ app/models/campaingfor_user.rb
@@ -2,4 +2,5 @@ class CampaingforUser < ApplicationRecord
   belongs_to :campaingvaccine
   belongs_to :user
   belongs_to :vaccination
+  belongs_to :turno
 end
--- app/models/turno.rb
+++ app/models/turno.rb
@@ -1,3 +1,4 @@
 class Turno < ApplicationRecord
   belongs_to :vaccination
+  has_many :campaingfor_users
 end
